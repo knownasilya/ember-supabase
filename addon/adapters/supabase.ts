6e4e172e54968ee304c6cb28e4a9ec3ecd448bc1
Charles Fries <charliefries@icloud.com>
2021-12-14 15:43:12 -0800

    Implement methods

--- addon/adapters/supabase.ts
+++ addon/adapters/supabase.ts
@@ -7,19 +7,104 @@ import type SupabaseService from 'ember-supabase/services/supabase';
 // @ts-ignore
 import type Store from '@ember-data/store';
 import type ModelRegistry from 'ember-data/types/registries/model';
+import type DS from 'ember-data';
+
+type ModelClass = any;
 
 export default class SupabaseAdapter extends JSONAPIAdapter {
   @service declare supabase: SupabaseService;
 
-  findAll<K extends keyof ModelRegistry>(
+  createRecord<K extends keyof ModelRegistry>(
     _store: Store,
-    type: ModelRegistry[K],
+    type: ModelClass,
+    snapshot: DS.Snapshot<K>
+  ): RSVP.Promise<any> {
+    return new RSVP.Promise((resolve, reject) => {
+      const serialized = this.serialize(snapshot, { includeId: true });
+      this.supabase.client
+        .from(pluralize(type.modelName))
+        .insert([serialized])
+        .then(({ data, error }) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(data);
+          }
+        });
+    });
+  }
+
+  updateRecord<K extends keyof ModelRegistry>(
+    _store: Store,
+    type: ModelClass,
+    snapshot: DS.Snapshot<K>
+  ): RSVP.Promise<any> {
+    return new RSVP.Promise((resolve, reject) => {
+      const serialized = this.serialize(snapshot, { includeId: true });
+      this.supabase.client
+        .from(pluralize(type.modelName))
+        .update([serialized])
+        .match({ id: snapshot.id })
+        .then(({ data, error }) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(data);
+          }
+        });
+    });
+  }
+
+  deleteRecord<K extends keyof ModelRegistry>(
+    _store: Store,
+    type: ModelClass,
+    snapshot: DS.Snapshot<K>
+  ): RSVP.Promise<any> {
+    return new RSVP.Promise((resolve, reject) => {
+      this.supabase.client
+        .from(pluralize(type.modelName))
+        .delete()
+        .match({ id: snapshot.id })
+        .then(({ data, error }) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(data);
+          }
+        });
+    });
+  }
+
+  findRecord<K extends keyof ModelRegistry>(
+    _store: Store,
+    type: ModelClass,
+    id: string,
+    _snapshot: DS.Snapshot<K>
+  ): RSVP.Promise<any> {
+    return new RSVP.Promise((resolve, reject) => {
+      this.supabase.client
+        .from(pluralize(type.modelName))
+        .select()
+        .match({ id })
+        .then(({ data, error }) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(data![0]);
+          }
+        });
+    });
+  }
+
+  findAll(
+    _store: Store,
+    type: ModelClass,
     _sinceToken: string,
     _snapshotRecordArray: any
   ): RSVP.Promise<any> {
     return new RSVP.Promise((resolve, reject) => {
       this.supabase.client
-        .from(pluralize((type as any).modelName))
+        .from(pluralize(type.modelName))
         .select()
         .then(({ data, error }) => {
           if (error) {
