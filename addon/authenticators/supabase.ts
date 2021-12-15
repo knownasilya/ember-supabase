a91611c53dfc4da01d009d9c07c02204aa5b34fc
Charles Fries <charliefries@icloud.com>
2021-12-15 10:24:22 -0800

    Authenticator

--- addon/authenticators/supabase.ts
+++ addon/authenticators/supabase.ts
@@ -3,9 +3,20 @@ import { inject as service } from '@ember/service';
 import BaseAuthenticator from 'ember-simple-auth/authenticators/base';
 import FastBootService from 'ember-cli-fastboot/services/fastboot';
 import SupabaseService from 'ember-supabase/services/supabase';
-import { Session } from '@supabase/gotrue-js';
 
+import type { Session } from '@supabase/gotrue-js';
 import type { SupabaseAuthClient } from '@supabase/supabase-js/dist/main/lib/SupabaseAuthClient';
+
+type User = any;
+type Provider = any;
+
+interface SBResponse {
+  session: Session | null;
+  user: User | null;
+  provider?: Provider;
+  url?: string | null;
+  error: Error | null;
+}
 
 export default class SupabaseAuthenticator extends BaseAuthenticator {
   @service declare supabase: SupabaseService;
@@ -18,7 +29,14 @@ export default class SupabaseAuthenticator extends BaseAuthenticator {
     callback: (auth: SupabaseAuthClient) => Promise<any>
   ): Promise<Record<string, unknown>> {
     const { auth } = this.supabase.client;
-    return await callback(auth);
+    const response = (await callback(auth)) as SBResponse;
+    return new Promise((resolve, reject) => {
+      if (response.error) {
+        reject(response.error);
+      } else {
+        resolve(response.user);
+      }
+    });
   }
 
   public async invalidate(): Promise<void> {
