7d3fde986c009afb2e24d5a7bbe18ba019835cbc
Charles Fries <charliefries@icloud.com>
2022-01-18 09:39:19 -0800

    dummy: Show auth state in application route

--- tests/dummy/app/controllers/application.ts
+++ tests/dummy/app/controllers/application.ts
@@ -1,6 +1,11 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
 
-export default class ApplicationController extends Controller {}
+import type Session from 'ember-simple-auth/services/session';
+
+export default class ApplicationController extends Controller {
+  @service declare session: Session;
+}
 
 // DO NOT DELETE: this is how TypeScript knows how to look up your controllers.
 declare module '@ember/controller' {
